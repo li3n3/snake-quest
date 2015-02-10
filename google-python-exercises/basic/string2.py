8c43aee1c5c65f182b77e52e6cf1f15302a79f58
Liene Verzemnieks <liene@simple.com>
2015-02-10 09:37:30 -0800

    string 2 start

--- google-python-exercises/basic/string2.py
+++ google-python-exercises/basic/string2.py
@@ -16,8 +16,12 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
-  # +++your code here+++
-  return
+  if len(s) >= 3:
+    if s[-3:] == 'ing':
+      s = s + 'ly'
+    else:
+      s = s + 'ing'
+  return s
 
 
 # E. not_bad
