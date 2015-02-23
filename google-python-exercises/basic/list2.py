1d80cc281590e16509a755ae18d39f5ec6c514da
Liene Verzemnieks <liene@simple.com>
2015-02-23 08:57:34 -0800

    remove_adjacent function works; add spoiler-alert whitespace

--- google-python-exercises/basic/list2.py
+++ google-python-exercises/basic/list2.py
@@ -13,8 +13,13 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-  # +++your code here+++
-  return
+  index = 0
+  while index < (len(nums) - 1):
+    if nums[index] == nums[index + 1]:
+      nums.pop(index + 1)
+    else:
+      index += 1
+  return nums
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
@@ -24,6 +29,18 @@ def remove_adjacent(nums):
 def linear_merge(list1, list2):
   # +++your code here+++
   return
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
