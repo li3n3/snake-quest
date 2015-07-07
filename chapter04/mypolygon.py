d11cd23a6b584773cce6f3722c2b9889ccd4fd63
Liene Verzemnieks <liene@simple.com>
2015-07-07 14:41:12 -0400

    draw the square with a function

--- chapter04/mypolygon.py
+++ chapter04/mypolygon.py
@@ -9,4 +9,15 @@ for i in range(4):
     fd(bob, 100)
     lt(bob)
 
+def square(t):
+    '''
+    takes a parameter t, a Turtle
+    uses that turtle to draw a square
+    '''
+    for i in range(4):
+        fd(t, 100)
+        lt(t)
+
+square(bob)
+
 wait_for_user()
