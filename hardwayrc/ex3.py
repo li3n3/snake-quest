0bd9dac3b2d6de61c2299173eb6c82f0ce1f42b1
Liene Verzemnieks <liene@simple.com>
2015-07-13 17:05:37 -0400

    lpthw: this time I will get through it

---
+++ hardwayrc/ex3.py
@@ -0,0 +1,28 @@
+print "I will now count my chickens:"
+
+# prints Hens, and the result of some math
+print "Hens", 25 + 30 / 6
+# more chickens, more math
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+# this is why we have computers do math
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# prints boolean for whether this statement is true
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# more booleans! yeah!
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
