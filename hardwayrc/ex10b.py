7548bd1d050833df88f71cca198891ab2c3301a7
Liene Verzemnieks <liene@simple.com>
2015-07-14 15:12:54 -0400

    editorializing

--- hardwayrc/ex10b.py
+++ hardwayrc/ex10b.py
@@ -1,3 +1,7 @@
+# I still can't quite figure out what this does, aside from something that
+# lasts for a long time, and some keypresses change the symbol in front of the
+# cursor, which is interesting to me
+
 while True:
     for i in ["/","-","|","\\","|"]:
         print "%s\r" % i,
