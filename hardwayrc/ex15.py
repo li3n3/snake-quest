496d0f527649ffe0e21217bcf4014b5a7e8c8a08
Liene Verzemnieks <liene@simple.com>
2015-07-15 12:06:56 -0400

    comment out argv stuff, use raw_input instead

--- hardwayrc/ex15.py
+++ hardwayrc/ex15.py
@@ -1,9 +1,14 @@
 # our friend, the argv
-from sys import argv
+# from sys import argv
 
 # gives things to argv: the script name, and whatever filename was specified
 # by the user
-script, filename = argv
+# script, filename = argv
+
+# use raw_input instead of a command-line argument. it's a bummer to not be
+# able to tab-complete the name of the file, but is a little clearer
+print "What's the filename?"
+filename = raw_input("> ")
 
 # opens the specified file, but doesn't do anything with it yet
 txt = open(filename)
