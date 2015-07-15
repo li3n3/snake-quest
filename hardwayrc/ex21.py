dff8686ccd5f450031a04ae8cc381337c1065750
Liene Verzemnieks <liene@simple.com>
2015-07-15 16:15:07 -0400

    exercise 21 yeah

---
+++ hardwayrc/ex21.py
@@ -0,0 +1,33 @@
+def add(a, b):
+    print "ADDING {0} + {1}".format(a, b)
+    return a + b
+
+def subtract(a, b):
+    print "SUBTRACTING {0} - {1}".format(a, b)
+    return a - b
+
+def multiply(a, b):
+    print "MULTIPLYING {0} * {1}".format(a, b)
+    return a * b
+
+def divide(a, b):
+    print "DIVIDING {0} / {1}".format(a, b)
+    return a / b
+
+
+print "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print "Age: {0}, Height: {1}, Weight: {2}, IQ: {3}".format(
+    age, height, weight, iq)
+
+# A puzzle for the extra credit; type it in anyway.
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
