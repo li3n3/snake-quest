dca8893ecf64048ecc4ab83042b39778ecdc549a
Liene Verzemnieks <liene@simple.com>
2015-07-15 13:22:00 -0400

    a bit more efficiency

--- hardwayrc/ex17.py
+++ hardwayrc/ex17.py
@@ -5,15 +5,14 @@ script, from_file, to_file = argv
 
 print "Copying from {0} to {1}".format(from_file, to_file)
 
-# we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
-print "The input file is {0} bytes long".format(len(indata))
-
-print "Does the output file exist? {0}".format(exists(to_file))
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+# caution? more like YOLO
+# print "The input file is {0} bytes long".format(len(indata))
+#
+# print "Does the output file exist? {0}".format(exists(to_file))
+# print "Ready, hit RETURN to continue, CTRL-C to abort."
+# raw_input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
@@ -21,4 +20,4 @@ out_file.write(indata)
 print "Alright, all done."
 
 out_file.close()
-in_file.close()
+# in_file.close()
