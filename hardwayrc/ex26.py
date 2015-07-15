7fe48ec269ebb6d1b3058770e5d4d4d994174b96
Liene Verzemnieks <liene@simple.com>
2015-07-15 18:00:35 -0400

    poop

--- hardwayrc/ex26.py
+++ hardwayrc/ex26.py
@@ -7,14 +7,14 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)
+def print_first_word(words):
     """Prints the first word after popping it off."""
-    word = words.poop(0)
+    word = words.pop(0)
     print word
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1
+    word = words.pop(-1)
     print word
 
 def sort_sentence(sentence):
