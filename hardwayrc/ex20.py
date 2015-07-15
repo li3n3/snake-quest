0b76f0a2bf133785113bb1a95803f378a22aff1f
Liene Verzemnieks <liene@simple.com>
2015-07-15 15:44:24 -0400

    whew, that's better

--- hardwayrc/ex20.py
+++ hardwayrc/ex20.py
@@ -26,8 +26,8 @@ print "Let's print three lines:"
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
