fd3f8805ab5f46e362d601a70167ae2e8dc3d3fb
Liene Verzemnieks <liene@simple.com>
2015-07-20 17:31:05 -0400

    slightly less absurd way for checking numbers

--- hardwayrc/ex35.py
+++ hardwayrc/ex35.py
@@ -4,7 +4,7 @@ def gold_room():
     print "This room is full of gold.  How much do you take?"
 
     choice = raw_input("> ")
-    if "0" in choice or "1" in choice:
+    if int(choice) > 0:
         how_much = int(choice)
     else:
         dead("Man, learn to type a number.")
