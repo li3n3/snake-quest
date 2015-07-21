902aed5ee1dc084d2c981aa1ccb727a0378febc5
Liene Verzemnieks <liene@simple.com>
2015-07-21 12:00:20 -0400

    reorganize functions, develop cat violence

--- hardwayrc/ex36.py
+++ hardwayrc/ex36.py
@@ -11,10 +11,10 @@ def start():
 
     if choice == "left":
         herb_garden()
-    elif choice == "center":
-        annuals()
     elif choice == "right":
         perennials()
+    elif choice == "center":
+        annuals()
     else:
         print "You're just a slug, huh?"
         print "I'll pick for you."
@@ -24,13 +24,26 @@ def start():
 def herb_garden():
     print "A fine aroma surrounds you."
     print "It appears you have found yourself in the herb garden."
+    print "Regrettably, so has a cat."
+    print "Do you turn around, hiss, or affix yourself to the cat?"
 
+    choice = raw_input("> ")
+
+    if "turn" in choice:
+        start()
+    elif choice == "hiss":
+        demise("The cat notes your small size, hisses back, hits you, and eats you.")
+
+def perennials():
+    print "suppp it's perennials"
 
 def annuals():
     print "eyyy the annuals are here"
 
-def perennials():
-    print "suppp it's perennials"
+
+def demise(reason):
+    print reason, "This was not such a good night."
+    exit(0)
 
 gardens = [herb_garden, annuals, perennials]
 
