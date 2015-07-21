a5a58b40aef83e80bd497ce9cc1b4240b95cbb5f
Liene Verzemnieks <liene@simple.com>
2015-07-21 11:44:57 -0400

    starting to sketch out text game

--- hardwayrc/ex36.py
+++ hardwayrc/ex36.py
@@ -1,1 +1,38 @@
-# this is where I write a game later. or maybe now?
+from sys import exit
+import random
+
+gardens = [herb_garden(), annuals(), perennials()]
+
+def start():
+    print "You find yourself on a garden path, dark and damp."
+    print "There are three garden beds here: left, right, and center."
+    print "Where would you like to crawl?"
+
+    choice = raw_input("> ")
+
+    if choice == "left":
+        herb_garden()
+    elif choice == "center":
+        annuals()
+    elif choice == "right":
+        perennials()
+    else:
+        print "You're just a slug, huh?"
+        print "I'll pick for you."
+        gardens[random.randint(0, 2)]
+
+def herb_garden():
+    print "A fine aroma surrounds you."
+    print "It appears you have found yourself in the herb garden."
+
+
+def annuals():
+    print "eyyy the annuals are here"
+
+def perennials():
+    print "suppp it's perennials"
+
+
+
+
+# print "You think to yourself, 'Yes, it is good to be a slug.'"
