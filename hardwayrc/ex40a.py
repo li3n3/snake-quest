0a1b923d347758a3b10a5fc8e877007a6b61b999
Liene Verzemnieks <liene@simple.com>
2015-07-23 17:26:58 -0400

    add more tiny pieces in, hope for no explosions

--- hardwayrc/ex40a.py
+++ hardwayrc/ex40a.py
@@ -1,4 +1,10 @@
 import mystuff
 
+mystuff = {'apple': "I AM APPLES!"}
+
 mystuff.apple()
 print mystuff.tangerine
+
+mystuff['apple'] # get apple from dict
+mystuff.apple() # get apple from the module
+mystuff.tangerine # same thing, it's just a variable
