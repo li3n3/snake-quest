9ee96534c47bf4324ccf2029aac2d77ca9a38537
Liene Verzemnieks <liene@simple.com>
2015-07-27 16:30:37 -0400

    many many many many notes on what things are

--- hardwayrc/ex42.py
+++ hardwayrc/ex42.py
@@ -2,48 +2,50 @@
 class Animal(object):
     pass
 
-## ??
+## Dog is-a Animal
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## self has-a attribute named name, set that to the parameter name
         self.name = name
 
-## ??
+## Cat is-a Animal
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## self has-a attribute named name, set that to the parameter name
         self.name = name
 
-## ??
+## Person is-a object
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## self has-a attribute named name, set that to the parameter name
         self.name = name
 
         ## Person has-a pet of some kind
         self.pet = None
 
-## ??
+## Employe is-a Person
 class Employee(Person):
 
     def __init__(self, name, salary):
-        ## ?? hmm what is this strange magic?
+        ## fancy way of calling this __init__. the superclass has-a Employee
+        ## (and self); it also has-a __init__, and call that. also a zillion
+        ## times less messy in Python 3
         super(Employee, self).__init__(name)
-        ## ??
-        self.salary = salry
+        ## self has-a salary, set it to salary
+        self.salary = salary
 
-## ??
+## Fish is-a object
 class Fish(object):
     pass
 
-## ??
+## Salmon is-a Fish
 class Salmon(Fish):
     pass
 
-## ??
+## Halibut is-a Fish
 class Halibut(Fish):
     pass
 
@@ -51,27 +53,26 @@ class Halibut(Fish):
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is-a Cat (well, duh)
 satan = Cat("Satan")
 
-## ??
+## mary is-a Person
 mary = Person("Mary")
 
-## ??
+## mary has-a pet, set to satan
 mary.pet = satan
 
-## ??
+## frank is-a Employee with parameters Frank and 120000
 frank = Employee("Frank", 120000)
 
-## ??
+## frank has-a pet
 frank.pet = rover
 
-## ??
+## flipper is-a Fish, which, sigh...
 flipper = Fish()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon()
 
-## ??
+## harry is-a Halibut, and likes alliteration
 harry = Halibut()
-
