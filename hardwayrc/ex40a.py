f6208375a2242a4afdf900b20dd27d86f664cd4e
Liene Verzemnieks <liene@simple.com>
2015-07-27 14:45:15 -0400

    piecing together things one, finding inconsistencies

--- hardwayrc/ex40a.py
+++ hardwayrc/ex40a.py
@@ -1,10 +1,37 @@
 import mystuff
 
-mystuff = {'apple': "I AM APPLES!"}
+mystuffs = {'apple': "I AM APPLES!"} # deliberately adding s to avoid collision
 
 mystuff.apple()
 print mystuff.tangerine
 
-mystuff['apple'] # get apple from dict
+mystuffs['apple'] # get apple from dict
 mystuff.apple() # get apple from the module
 mystuff.tangerine # same thing, it's just a variable
+
+
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print "I AM CLASSY APPLES!"
+
+thing = MyStuff()
+thing.apple()
+print thing.tangerine
+
+
+# look at all these ways to get things from things:
+# dict style
+mystuffs['apples'] # also this sudden pluralization is interesting
+
+# module style
+mystuff.apples()
+print mystuff.tangerine
+
+# class style
+thing = MyStuff()
+thing.apples()
+print thing.tangerine
